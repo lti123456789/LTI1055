ec5537d8216850235c55822895213aef18b8cf4d
lti123456789 <charithaghanta2002@gmail.com>
2022-09-19 17:54:40 +0530

    c# projects

---
+++ p1/p1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Transactions;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static int MyMethod( int x, int y)
+        {
+            return y+x;
+        }
+    
+       
+             static void Main(string[] args)
+             {
+                Console.WriteLine(MyMethod(3,4));
+             }
+    }
+}
